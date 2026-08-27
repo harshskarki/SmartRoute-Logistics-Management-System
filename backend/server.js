0610edf46d07db86aa68af57bce442a36886657e
harshskarki <harshskarki@gmail.com>
2026-08-27 11:34:34 +0530

    Project restructured with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚚 SmartRoute Backend Running");
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "running",
+    service: "SmartRoute Backend",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `🚀 SmartRoute Server running on port ${PORT}`
+  );
+});
